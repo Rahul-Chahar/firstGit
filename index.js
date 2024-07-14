bba18d7a2f3bf9998c32186ee9fb705410d61860
Rahul-Chahar <rahulchahar.5412381@gmail.com>
2024-07-14 23:11:05 +0530

    use getElementsByTagName

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ fruit[2].style.backgroundColor = 'yellow';
 for(let i = 0; i < fruit.length; i++){
   fruit[i].style.fontWeight = 'bold';
 }
+
+const listItems = document.getElementsByTagName('li');
+listItems[4].style.color = 'blue';
+
+for(let i = 0; i < listItems.length; i++){
+  listItems[i].style.fontStyle = 'italic';
+}
